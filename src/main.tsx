6670dd01c9da4aaa656f2a5ffde19b0ea346be52
Kevin Gonzalez <105946335+revkelo@users.noreply.github.com>
2026-04-08 16:27:13 -0500

    Deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-﻿import React from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import App from "./app/App";
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
+    <SpeedInsights />
   </React.StrictMode>
 );
